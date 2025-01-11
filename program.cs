5ec814e25eef4b54577938dfc0031cef0b927dfa
Chris <37301037+bollins7@users.noreply.github.com>
2025-01-11 15:20:36 -0600

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MyFirstProgram
+{   
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I like pizza!");
+            Console.WriteLine("Its really good");
+        }
+    }
+}    
